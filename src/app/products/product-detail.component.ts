98dd1fbb211d4571c65f196cec832e27cb8248af
Nick Thacker <NThacker@equitable.ca>
2019-07-30 17:02:25 -0400

    Refactored to grab the product detail via the web service.  Also put the details into view

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,24 +10,22 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  product: IProduct;
+  errorMessage: string = '';
+  product: IProduct | undefined;
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    // TODO: Change this to grab the products from the web service based off of id
-    this.product = {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    const param = +this.route.snapshot.paramMap.get('id');
+
+    if (param) {
+      const id = +param;
+      this.productService.getProduct(id).subscribe(
+        product => this.product = product,
+        error => this.errorMessage = <any>error
+      );
     }
   }
 
