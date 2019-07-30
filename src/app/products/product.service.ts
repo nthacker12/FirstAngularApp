d32dc515dedc1e8fd07d26ad19d4563a898a703e
Nick Thacker <NThacker@equitable.ca>
2019-07-30 14:36:44 -0400

    Modified service to get products via an HTTP request, and removed hard coded products

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,43 +1,33 @@
-import { IProduct } from './product';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { IProduct } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productUrl = 'api/products/products.json';
 
-  getProducts(): IProduct[] {
-    return [
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2016",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      },
-      {
-          "productId": 5,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2016",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-      },
-      {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2015",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-      }
-    ];
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+                    .pipe(tap(data => console.log('All: ' + JSON.stringify(data))), catchError(this.handleError));
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
