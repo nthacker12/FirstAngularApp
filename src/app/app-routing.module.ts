5a86495372e69759d1bcfb232cc23c67fb3a17b7
Nick Thacker <NThacker@equitable.ca>
2019-07-31 10:45:40 -0400

    Refactored app into feature modules

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductRoutingModule } from './products/product-routing.module';
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductRoutingModule,
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
